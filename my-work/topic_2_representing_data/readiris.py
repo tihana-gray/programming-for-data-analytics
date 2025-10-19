ae0ac86e7ad2c3cec210932e2fd2faa47b2c91fb
Tihana <tihanagray64@gmail.com>
2025-10-19 13:59:04 +0100

    learning and assignment 3 work in progress

---
+++ my-work/topic_2_representing_data/readiris.py
@@ -0,0 +1,18 @@
+# case study of iris data set
+# area of sepals and petals
+# Author: Tihana Gray
+
+import csv
+
+FILENAME= "iris.csv"
+DATADIR = "C:/Users/tihan/OneDrive/Desktop/ATU/programming-for-data-analytics/data/"
+
+with open (DATADIR + FILENAME, "rt") as fp:
+    reader = csv.reader(fp, delimiter="," , quoting=csv.QUOTE_NONE)
+    
+    for line in reader:
+        sepal_size= float(line[0]) * float(line[1])
+        petal_size= float(line[2]) * float(line[3])
+        line.append(sepal_size)
+        line.append(petal_size)
+        print (line)
