73648c6a28fc4e8aaa967a605875fabc33ae7ac9
Tihana <tihanagray64@gmail.com>
2025-11-11 19:28:51 +0000

    coursework and assignment06 start

---
+++ my-work/topic_4_cleaning_data_and_regular_expressions/Lab_04.01._regex/03_SomeCode(1).py
@@ -0,0 +1,17 @@
+# This is code that will find some text in an access file
+# Author: Tihana Gray
+
+import re
+regex = "\[.*\]"
+
+filename = "C:\\Users\\tihan\\OneDrive\\Desktop\\ATU\\programming-for-data-analytics\\my-work\\topic_4_cleaning_data_and_regular_expressions\\Lab_04.01._regex\\smallerAccess.log.txt"
+
+with open(filename) as inputFile:
+    for line in inputFile:
+        foundTextList = re.findall(regex, line)
+        if (len(foundTextList)!= 0):
+            #print(foundTextList)
+            foundText = foundTextList[0]
+            print(foundText)
+            # if I did not want the [] at the beginning and end
+            print(foundText[1:-1])
